63c04c627601ec5e707b52db5d4a37f65f226d79
Slavomir <gagliardetto@users.noreply.github.com>
2019-07-14 15:01:48 +0200

    first commit

---
+++ example/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/gagliardetto/listfile"
+)
+
+func main() {
+	lst, err := listfile.New("./example.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer lst.Close()
+
+	for iPar := 0; iPar < 5; iPar++ {
+		ii := strconv.Itoa(iPar)
+		go func() {
+			for i := 0; i < 10; i++ {
+				err = lst.Append(ii + "item" + strconv.Itoa(i))
+				if err != nil {
+					panic(err)
+				}
+			}
+		}()
+	}
+	time.Sleep(time.Second)
+	lst.IterateLines(func(line string) bool {
+		fmt.Println(line)
+		return true
+	})
+
+	for i := 990; i < 999; i++ {
+		err = lst.Append("hello world after reading")
+		if err != nil {
+			panic(err)
+		}
+	}
+}
---
+++ listfile.go
@@ -0,0 +1,102 @@
+package listfile
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"sync"
+)
+
+type ListFile struct {
+	file *os.File
+	mu   *sync.RWMutex
+}
+
+// Append appends one or more strings adding a newline to each
+func (lf *ListFile) Append(items ...string) error {
+	lf.mu.Lock()
+	defer lf.mu.Unlock()
+
+	for _, item := range items {
+		_, err := lf.file.WriteString(item + "\n")
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (lf *ListFile) Len() int {
+	err := lf.file.Sync()
+	if err != nil {
+		// TODO: not panic??
+		panic(err)
+	}
+
+	info, err := lf.file.Stat()
+	// TODO: consider error?
+	if err != nil {
+		// TODO: not panic??
+		panic(err)
+	}
+
+	return int(info.Size())
+}
+func (lf *ListFile) LenInt64() int64 {
+	return int64(lf.Len())
+}
+
+// IterateLines iterates on the lines of the list
+func (lf *ListFile) IterateLines(iterator func(line string) bool) error {
+	lf.mu.Lock()
+	defer lf.mu.Unlock()
+
+	sectionReader := io.NewSectionReader(lf.file, 0, lf.LenInt64())
+
+	scanner := bufio.NewScanner(sectionReader)
+	for scanner.Scan() {
+		doContinue := iterator(scanner.Text())
+		if !doContinue {
+			return nil
+		}
+		err := scanner.Err()
+		if err != nil {
+			return fmt.Errorf("error while iterating over scanner: %s", err)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return fmt.Errorf("error of scanner: %s", err)
+	}
+
+	return nil
+}
+
+func (lf *ListFile) Close() error {
+	err := lf.file.Sync()
+	if err != nil {
+		return err
+	}
+
+	err = lf.file.Close()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func New(filepath string) (*ListFile, error) {
+	file, err := os.OpenFile(filepath, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0644)
+	if err != nil {
+		return nil, fmt.Errorf("error while OpenFile: %s", err)
+	}
+
+	lf := &ListFile{
+		file: file,
+		mu:   &sync.RWMutex{},
+	}
+
+	return lf, nil
+}
